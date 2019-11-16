596345f78fc49b924a65526596e3fc144f3a1db7
Adijav <agjavali@gmail.com>
2019-11-15 21:22:40 -0600

    added burger builder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Aux from '../../hoc/Aux';
+import BurgerBuilder from '../../containers/BurgerBuilder/BurgerBuilder';
+
 
 const Layout = (props) => (
-    <div>toolbar, sideDrawer, backdrop</div>
-    <main>
-        {props.children}
-    </main>
+    <Aux>
+        <div>toolbar, sideDrawer, backdrop</div>
+        <main>
+            <BurgerBuilder/>
+            {props.children}
+        </main>
+    </Aux>
 )
+
+export default Layout;
